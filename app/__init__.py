aebb25841e801fb96776e8c9a52c2b1eda0115a8
aidenblampain <22971029@student.uwa.edu.au>
2026-05-07 13:54:16 +0800

    removed useless import

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config 
 from flask_login import LoginManager
-from config import Config 
 
 app = Flask(__name__)
 app.config.from_object(Config)
