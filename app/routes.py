d3f6b9e3cb34f4d265ef19d1d82dbe3f39e30162
Angela Hewitt <24223498@student.uwa.edu.au>
2026-05-10 20:07:07 +0800

    Added the Recipe Table to the migration.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, request, redirect, url_for
-from app import app
+from app import app, db
 from flask_login import login_user, logout_user
-from app.models import User
-from app import db
+from app.models import User, Recipe
 
 @app.route('/')
 @app.route('/index')
--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_database_location
     # Added the os.environ.get('SECRET_KEY') based on Flask Mega Tutorial
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret999'
