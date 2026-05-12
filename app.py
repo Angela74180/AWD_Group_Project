760c0f386f1d3ac8e7e78c886c96e4d597dcc4a6
ix-cyn <icyn27@gmail.com>
2026-05-12 21:19:13 +0800

    fixed merge conflicts

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app import app, db
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db}
-=======
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -20,4 +8,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> e9a5f701777a72781aad79c3ba0d09b662b29bb3
