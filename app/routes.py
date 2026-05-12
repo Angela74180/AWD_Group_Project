eb052b5543e45b0c0bd05e8b4d1c5737470d6b0c
Angela Hewitt <24223498@student.uwa.edu.au>
2026-05-12 22:20:33 +0800

    Made it so that photos will be included if you decide to edit a recipe.

--- app/routes.py
+++ app/routes.py
@@ -332,26 +332,22 @@ def publish_recipe():
 
 @app.route('/create_recipe/<recipe_num>')
 def create_recipe(recipe_num):
+    ############### You will nedd to actually check for a User
+    signed_in = True
+
+
+    authorId = session.get('authorId')
+    author = User.query.filter_by(id=authorId).first().username
 
     empty_dict = {
+        "id": 0,
+        "recipeId": 0,
         "allowRatings": True,
         "allowReviews": True,
-        "appliances": [
-            {
-                "desc": "",
-                "extraData": "",
-                "name": ""
-            }
-        ],
-        "author": "",
-        "ingredients": [
-            {
-                "desc": "",
-                "name": "",
-                "quantity": "",
-                "units": ""
-            }
-        ],
+        "appliances": [{"desc": "", "extraData": "", "name": ""}],
+        "authorId": authorId,
+        "author": author,
+        "ingredients": [{"desc": "", "name": "", "quantity": "", "units": ""}],
         "recipeCoverImage": "",
         "recipeDescription": "",
         "recipeDifficulty": "Simple",
@@ -359,304 +355,89 @@ def create_recipe(recipe_num):
         "recipeType": "Breakfast",
         "serves": "",
         "status": "Draft",
-        "steps": [
-            {
-                "desc": "",
-                "name": "",
-                "photo": ""
-            }
-        ],
-        "tagList": [
-            ""
-        ],
-        "timeList": {
-            "cookingTime": [
-                "",
-                ""
-            ],
-            "prepTime": [
-                "",
-                ""
-            ],
-            "totalTime": [
-                "",
-                ""
-            ]
-        },
+        "steps": [{"desc": "", "name": "", "photo": ""}],
+        "tagList": [""],
+        "timeList": {"cookingTime": ["",""], "prepTime": ["",""], "totalTime": ["",""]},
         "timeSplit": False,
-        "visibility": "Private"
+        "visibility": "Private",
+        "bookmark_on": False,
+        "cart_on": False,
+        "signed_in": signed_in,
+        "allowed_to_view": True
     }
-
-
-
-    pancake_dict = {
-        "author": "Angela74180",
-        "recipeName": "My Pancake Recipe",
-        "recipeType": "Dessert",
-        "recipeDifficulty": "Simple",
-        "tagList": [
-            "Vegetarian"
-        ],
-        "timeSplit": False,
-        "timeList": {
-            "totalTime": [
-                1,
-                0
-            ],
-            "prepTime": [
-                0,
-                0
-            ],
-            "cookingTime": [
-                0,
-                0
-            ]
-        },
-        "recipeDescription": "This is a recipe that create approximately 12 crepe like pancakes. They are great to have with your favourite toppings. I would recommend topping them with a sprinkle of sugar and a drizzle of lemon if you aren't sure what to add.",
-        "recipeCoverImage": "https://www.jocooks.com/wp-content/uploads/2018/12/crepes-1-8.jpg",
-        "visibility": "Friends_Only",
-        "allowRatings": False,
-        "allowReviews": False,
-        "serves": 4,
-        "status": "Draft",
-        "ingredients": [
-            {
-                "name": "Plain Flour",
-                "quantity": "115",
-                "units": "g",
-                "desc": ""
-            },
-            {
-                "name": "Eggs",
-                "quantity": "1",
-                "units": "\"Whole\"",
-                "desc": ""
-            },
-            {
-                "name": "Milk",
-                "quantity": "250",
-                "units": "mL",
-                "desc": ""
-            },
-            {
-                "name": "Salt",
-                "quantity": "1",
-                "units": "\"Pinch\"",
-                "desc": ""
-            },
-            {
-                "name": "Butter",
-                "quantity": "20",
-                "units": "g",
-                "desc": ""
-            },
-            {
-                "name": "Castor Sugar",
-                "quantity": "0.25",
-                "units": "Cup",
-                "desc": ""
-            },
-            {
-                "name": "Lemon",
-                "quantity": "1",
-                "units": "\"Whole\"",
-                "desc": "This is optional and is only required if you want to use it as a topping."
-            }
-        ],
-        "appliances": [
-            {
-                "name": "Stove",
-                "extraData": "",
-                "desc": ""
-            }
-        ],
-        "steps": [
-            {
-                "name": "Preparing The Batter",
-                "desc": "Sift flour and salt into a bowl. Create a well in the centre and drop in the egg.",
-                "photo": ""
-            },
-            {
-                "name": "Preparing The Batter",
-                "desc": "Add half the liquids (includes butter) in small increments at a time and mix until smooth.",
-                "photo": ""
-            },
-            {
-                "name": "Preparing The Batter",
-                "desc": "Beat in the remaining liquid and stir until it has the consistency of thin cream.",
-                "photo": ""
-            },
-            {
-                "name": "Cooking",
-                "desc": "Pour a small amount (only enough to coat the bottom of the pan) into a frypan over medium heat.",
-                "photo": ""
-            },
-            {
-                "name": "Cooking",
-                "desc": "Flip the pancake over once the underside is mostly cooked.",
-                "photo": ""
-            },
-            {
-                "name": "Cooking",
-                "desc": "Once the pancake is cooked, serve it with your choice of toppings.",
-                "photo": "https://www.jocooks.com/wp-content/uploads/2018/12/crepes-1-8.jpg"
-
-            }
-        ]
-    }
-
-
-
-    tuna_mornay_dict = {
-        "allowRatings": True,
-        "allowReviews": True,
-        "appliances": [
-            {
-                "desc": "",
-                "extraData": "",
-                "name": "Oven"
-            },
-            {
-                "desc": "",
-                "extraData": "",
-                "name": "Stove"
-            }
-        ],
-        "author": "Angela74180",
-        "ingredients": [
-            {
-                "desc": "The tin is 200g.",
-                "name": "Tuna",
-                "quantity": "1",
-                "units": "\"Tin\""
-            },
-            {
-                "desc": "",
-                "name": "Lemon Juice",
-                "quantity": "1",
-                "units": "tbsp"
-            },
-            {
-                "desc": "",
-                "name": "Garlic Salt",
-                "quantity": "0.5",
-                "units": "tsp"
-            },
-            {
-                "desc": "AKA 2 tbsps.",
-                "name": "Plain Flour",
-                "quantity": "0.25",
-                "units": "Cup"
-            },
-            {
-                "desc": "",
-                "name": "Milk",
-                "quantity": "2",
-                "units": "Cup"
-            },
-            {
-                "desc": "",
-                "name": "Crushed Potato Crisps",
-                "quantity": "1",
-                "units": "Cup"
-            },
-            {
-                "desc": "",
-                "name": "Onion Flakes",
-                "quantity": "1",
-                "units": "tsp"
-            },
-            {
-                "desc": "",
-                "name": "Chopped Parsley",
-                "quantity": "2",
-                "units": "tbsp"
-            },
-            {
-                "desc": "",
-                "name": "Margarine",
-                "quantity": "0.25",
-                "units": "Cup"
-            },
-            {
-                "desc": "",
-                "name": "Cayenne Pepper",
-                "quantity": "1",
-                "units": "\"Pinch\""
-            },
-            {
-                "desc": "",
-                "name": "Crumbled Bread",
-                "quantity": "2",
-                "units": "\"Slices\""
-            }
-        ],
-        "recipeCoverImage": "https://tse1.mm.bing.net/th/id/OIP.Z5_7sbpWkaWZJkN6qd17GgHaE8?rs=1&pid=ImgDetMain&o=7&rm=3",
-        "recipeDescription": "This is a recipe that serves approximately 8. It is great to have with a side of vegetables or is fine on its own.",
-        "recipeDifficulty": "Intermediate",
-        "recipeName": "Tuna Mornay",
-        "recipeType": "Dinner",
-        "serves": 8,
-        "status": "Draft",
-        "steps": [
-            {
-                "desc": "Combine Drained and flaked tuna, onion flakes, lemon juice, parsley and garlic salt. ",
-                "name": "",
-                "photo": ""
-            },
-            {
-                "desc": "In a pot over the stove, melt the marg and blend in flour and seasonings. Add the milk very gradually and cook until it is thick and smooth, stirring constantly.",
-                "name": "White Sauce",
-                "photo": ""
-            },
-            {
-                "desc": "Fold in the tuna mixture and breadcrumbs and place the mixture in a casserole dish.",
-                "name": "",
-                "photo": ""
-            },
-            {
-                "desc": "Cover with potato crisps.",
-                "name": "",
-                "photo": ""
-            },
-            {
-                "desc": "Cook at 400 degrees F for 20 mins.",
-                "name": "Cooking It",
-                "photo": "https://tse1.mm.bing.net/th/id/OIP.Z5_7sbpWkaWZJkN6qd17GgHaE8?rs=1&pid=ImgDetMain&o=7&rm=3"
-            }
-        ],
-        "tagList": [
-            "Pescatarian",
-            "OTHER"
-        ],
-        "timeList": {
-            "cookingTime": [
-                0,
-                0
-            ],
-            "prepTime": [
-                0,
-                0
-            ],
-            "totalTime": [
-                0,
-                40
-            ]
-        },
-        "timeSplit": False,
-        "visibility": "Private"
-    }
-
-    recipes_dict = {}
 
     if recipe_num == "0":
-        recipes_dict = empty_dict
-    elif recipe_num == "1":
-        recipes_dict = pancake_dict
-    elif recipe_num == "2":
-        recipes_dict = tuna_mornay_dict
-    else:
-        return "Recipe not found", 404
+        return render_template('create_recipe.html', recipe_details_dict=empty_dict) 
+
+        
+    allowed_to_view=True
+
+    userId = session.get('authorId')
+    user = User.query.filter_by(id=userId).first()
+
+    recipe = Recipe.query.filter_by(id=recipe_num).first()
+
+    bookmark = Bookmark.query.filter_by(user_id=userId, recipe_id=recipe.id).first()
+    cart = ShoppingList.query.filter_by(user_id=userId, recipe_id=recipe.id).first()
+    bookmark_on = True
+    if not bookmark:
+        bookmark_on = False
+    cart_on = True
+    if not cart:
+        cart_on = False
+    
+
+    tag_list = []
+    for recipeTag in recipe.tags:
+        tag_list.append(Tag.query.filter_by(id=recipeTag.tag_id).first().name)
+
+    appliances = []
+    for recipeAppliance in recipe.appliances:
+        appliance = Appliance.query.filter_by(id=recipeAppliance.appliance_id).first()
+        recipe_appliance = RecipeAppliance.query.filter_by(appliance_id=appliance.id, recipe_id=recipe.id).first()
+        appliance_dict = {
+            "name": appliance.name,
+            "extraData": recipe_appliance.extra_data,
+            "desc": recipe_appliance.desc
+        }
+        appliances.append(appliance_dict)
+
+
+    ingredients = []
+    for recipeIngredient in recipe.ingredients:
+        ingredient = Ingredient.query.filter_by(id=recipeIngredient.ingredient_id).first()
+        recipe_ingredient = RecipeIngredient.query.filter_by(ingredient_id=ingredient.id, recipe_id=recipe.id).first()
+
+        quantity = float(recipe_ingredient.quantity)
+
+        # If it's a whole number, return int
+        if quantity.is_integer():
+            quantity = int(quantity)
+
+
+        ingredient_dict = {
+            "name": ingredient.name,
+            "quantity": quantity,
+            "units": recipe_ingredient.units,
+            "desc": recipe_ingredient.desc
+        }
+        ingredients.append(ingredient_dict)
+
+
+    steps = []
+    for recipeStep in recipe.steps:
+        # step = Step.query.filter_by(id=recipeStep.step_id).first()
+        step = Step.query.filter_by(id=recipeStep.id, recipe_id=recipe.id).first()
+        step_dict = {
+            "name": step.name,
+            "step_number": step.step_number,
+            "desc": step.desc,
+            "photo": step.photo
+        }
+        steps.append(step_dict)
+
+
+    recipes_dict = make_recipe_dict(recipe, author, tag_list, appliances, ingredients, steps, bookmark_on, cart_on, signed_in=signed_in, allowed_to_view=allowed_to_view)
 
     return render_template('create_recipe.html', recipe_details_dict=recipes_dict) 
 
