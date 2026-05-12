18137e54583607838be7d53a2212e3c461595e72
Angela Hewitt <24223498@student.uwa.edu.au>
2026-05-12 16:21:38 +0800

    Made the saved page show the recipes that the current user has bookmarked.

--- app/routes.py
+++ app/routes.py
@@ -26,6 +26,42 @@ def shopping_list():
     for shopping_list in shopping_lists:
         recipe_id = shopping_list.recipe_id
         recipe = Recipe.query.filter_by(id=recipe_id).first()
+
+        author = User.query.filter_by(id=recipe.author_id).first().username
+        bookmark = Bookmark.query.filter_by(user_id=userId, recipe_id=recipe.id).first()
+        cart = ShoppingList.query.filter_by(user_id=userId, recipe_id=recipe.id).first()
+
+        bookmark_on = True
+        if not bookmark:
+            bookmark_on = False
+
+        cart_on = True
+        if not cart:
+            cart_on = False
+
+        tag_list = []
+        
+        for recipeTag in recipe.tags:
+            tag_list.append(Tag.query.filter_by(id=recipeTag.tag_id).first().name)
+
+        recipes_list.append(make_recipe_banner_dict(recipe, author, tag_list, bookmark_on, cart_on))
+
+    return render_template("shopping_list.html", username=user.username, cartRecipes=recipes_list[::-1])
+
+
+@app.route("/saved")
+def saved():
+    userId = session.get('authorId')
+    user = User.query.filter_by(id=userId).first()
+
+    bookmarks = user.bookmarks
+
+
+    recipes_list = []
+    for bookmark in bookmarks:
+        print(bookmark)
+        recipe_id = bookmark.recipe_id
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
         
         author = User.query.filter_by(id=recipe.author_id).first().username
         bookmark = Bookmark.query.filter_by(user_id=userId, recipe_id=recipe.id).first()
@@ -46,7 +82,7 @@ def shopping_list():
 
         recipes_list.append(make_recipe_banner_dict(recipe, author, tag_list, bookmark_on, cart_on))
 
-    return render_template("shopping_list.html", username=user.username, userRecipes=recipes_list[::-1])
+    return render_template("savedPage.html", username=user.username, savedRecipes=recipes_list[::-1])
 
 
 @app.route('/publish_recipe', methods=["POST"])
---
+++ app/static/Saved_Recipes/populateSaved.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    populate(recipes_list);
+});
+
+function populate(recipes_list) {
+    let container = document.getElementById("recipe_banner_div");
+    container.innerHTML = `Looks like you haven't saved any recipes yet...`;
+    if (recipes_list != ""){
+        container.innerHTML = ``;
+        for (let recipe_dict of recipes_list){
+            container.appendChild(makeRecipeBanner(recipe_dict));
+        }
+    }
+}
