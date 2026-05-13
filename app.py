a3e7fb8f0e7c458377d47ba61a576a051aab9adc
ix-cyn <icyn27@gmail.com>
2026-05-13 16:34:39 +0800

    Made changes to app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
 
-app = Flask(__name__)
-
-@app.route("/profile")
-def home():
-    return render_template("profile.html")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db}
