3b4996c06a6fd3ec26ae3ea6ce5b7e2887216c7c
ix-cyn <icyn27@gmail.com>
2026-05-14 14:24:43 +0800

    made uploadAvatar code more readable

--- app/static/Profile/uploadAvatar.js
+++ app/static/Profile/uploadAvatar.js
@@ -3,37 +3,37 @@ document.addEventListener("DOMContentLoaded", function() {
   let avatarClickTargets = ["avatarWrap", "uploadPhotoBtn", "avatarSmall"];
 
   for (let id of avatarClickTargets) {
-      document.getElementById(id).addEventListener("click", () => document.getElementById("avatar-input").click());
+    document.getElementById(id).addEventListener("click", () => document.getElementById("avatar-input").click());
   }
 
-    document.getElementById("avatar-input").addEventListener("change", e => {
-        let file = e.target.files[0];
-        if (!file) return;
+  document.getElementById("avatar-input").addEventListener("change", e => {
+    let file = e.target.files[0];
+      if (!file) return;
 
-        let reader = new FileReader();
-        reader.onload = ev => syncAvatars(ev.target.result);
-        reader.readAsDataURL(file);
+      let reader = new FileReader();
+      reader.onload = ev => syncAvatars(ev.target.result);
+      reader.readAsDataURL(file);
 
-        document.getElementById("avatar-input").value = "";
-    });
+      document.getElementById("avatar-input").value = "";
+  });
 
-    function syncAvatars(imageData) {
-        let avatarDisplay = document.getElementById("avatarDisplay");
+  function syncAvatars(imageData) {
+    let avatarDisplay = document.getElementById("avatarDisplay");
 
-        if (avatarDisplay) {
-            if (avatarDisplay.tagName === "IMG") {
-                avatarDisplay.src = imageData;
-            } else {
-                let img = document.createElement("img");
-                img.src = imageData;
-                img.id = "avatarDisplay";
-                img.className = "avatar-display-img";
-                img.alt = "Profile Picture";
-                avatarDisplay.replaceWith(img);
-            }
-        }
+    if (avatarDisplay) {
+      if (avatarDisplay.tagName === "IMG") {
+        avatarDisplay.src = imageData;
+      } else {
+        let img = document.createElement("img");
+        img.src = imageData;
+        img.id = "avatarDisplay";
+        img.className = "avatar-display-img";
+        img.alt = "Profile Picture";
+        avatarDisplay.replaceWith(img);
+      }
+    }
 
-        document.getElementById("avatarSmall").innerHTML = `<img src="${imageData}" class="avatar-small-img">`;
+    document.getElementById("avatarSmall").innerHTML = `<img src="${imageData}" class="avatar-small-img">`;
 
         fetch("/upload_avatar", {
             method: "POST",
