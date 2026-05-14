4b6ec53a3df2379b9fb80d13a502acac65ab8aae
aidenblampain <22971029@student.uwa.edu.au>
2026-05-14 13:49:12 +0800

    added unittests

--- app/test/test_data.py
+++ app/test/test_data.py
@@ -1,13 +1,17 @@
 import unittest
 from app import create_app, db
 from config import TestConfig
-from app.models import User
+from app.models import User, Recipe
+from flask_login import login_user
 
 class BasicTests(unittest.TestCase):
 
     def setUp(self):
-        testApp = create_app(TestConfig)
-        self.app_context = testApp.app_context()
+        self.app = create_app(TestConfig)
+
+        self.client = self.app.test_client()
+
+        self.app_context = self.app.app_context()
         self.app_context.push()
 
         db.create_all()
@@ -34,3 +38,110 @@ class BasicTests(unittest.TestCase):
 
         self.assertTrue(s.check_password("bubbles"))
         self.assertFalse(s.check_password("rumbles"))
+
+    def test_signup(self):
+        response = self.client.post(
+            "/signup",
+            data={
+                "username": "testuser",
+                "email": "test@test.com",
+                "password": "abc123"
+            },
+            follow_redirects=True
+        )
+
+        user = User.query.filter_by(username="testuser").first()
+
+        self.assertIsNotNone(user)
+
+    def test_duplicate_signup(self):
+        user = User(username="test", email="test@test.com")
+        user.set_password("abc")
+
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.client.post(
+            "/signup",
+            data={
+                "username": "test",
+                "email": "another@test.com",
+                "password": "123"
+            }
+        )
+
+        self.assertIn(b"already exists", response.data)
+
+    def test_create_recipe(self):
+
+        user = User(username="chef", email="chef@test.com")
+        user.set_password("abc")
+
+        db.session.add(user)
+        db.session.commit()
+
+        with self.client.session_transaction() as sess:
+            sess["authorId"] = user.id
+
+        response = self.client.post(
+            "/publish_recipe",
+            data={
+                "recipe_name": "Toast",
+                "recipeType": "Breakfast",
+                "recipeDifficulty": "Simple",
+                "serves": 1,
+                "Description": "Bread",
+                "coverPhoto": "",
+                "visibility": "Public",
+
+                "ingredientName": ["Bread"],
+                "ingredientQuantity": ["2"],
+                "ingredientUnits": ["Slices"],
+                "ingredientDescription": [""],
+
+                "tagName": ["Quick"],
+
+                "applianceName": ["Toaster"],
+                "extraData": [""],
+                "applianceDescription": [""],
+
+                "stepName": ["Toast bread"],
+                "stepDescription": ["Put bread in toaster"],
+                "stepPhoto": [""]
+            },
+            follow_redirects=True
+        )
+
+        recipe = Recipe.query.filter_by(name="Toast").first()
+
+        self.assertIsNotNone(recipe)
+
+    def test_login(self):
+        response = self.client.post(
+            "/login",
+            data={
+                "username": "testuser",
+                "password": "bubbles"
+            },
+            follow_redirects=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_password_success(self):
+
+        self.client.post('/login', data={
+            "username": "testuser",
+            "password": "bubbles"
+        }, follow_redirects=True)
+
+        response = self.client.post(
+            "/update_password",
+            json={
+                "current": "bubbles",
+                "new": "newpass123"
+            }
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.get_json()["success"])
